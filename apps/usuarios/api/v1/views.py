ba7891d5840cc9c316439257e2de744c167da9f2
LopsanAMO <lopsan.molina@gmail.com>
2018-04-16 13:39:43 -0500

    minor fixes

--- apps/usuarios/api/v1/views.py
+++ apps/usuarios/api/v1/views.py
@@ -65,12 +65,12 @@ class FiscalAPIView(APIView):
         """Get fiscal information from user
         """
         req_inf = RequestInfo()
-        rfc = request.data.get('rfc', None)
+        rfc = request.GET.get('rfc', None)
         user = request.user
         try:
             if rfc is not None:
                 user = Fiscal.objects.get(rfc=rfc).user
-            return Response(UserInfoSerializer(user).data)
+            return Response(UserInfoSerializer(suser).data)
         except ObjectDoesNotExist as e:
             return req_inf.status(e.args[0], status.HTTP_404_NOT_FOUND)
         except Exception as e:
